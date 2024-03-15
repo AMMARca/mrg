10cf8514f636843c5d2933fdea99cc25b3c6735e
AMMARca <68115629+AMMARca@users.noreply.github.com>
2024-03-15 04:56:46 +0200

    Update config.py

--- config.py
+++ config.py
@@ -2,14 +2,14 @@ import os
 
 
 class Config(object):
-    API_HASH = os.environ.get("API_HASH")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
-    TELEGRAM_API = os.environ["TELEGRAM_API"]
-    OWNER = os.environ.get("OWNER")
-    OWNER_USERNAME = os.environ.get("OWNER_USERNAME")
-    PASSWORD = os.environ.get("PASSWORD")
-    DATABASE_URL = os.environ.get("DATABASE_URL")
-    LOGCHANNEL = os.environ.get("LOGCHANNEL")  # Add channel id as -100 + Actual ID
+    API_HASH = os.environ.get("d3f57b56ad8463fc96cc969da6586381")
+    BOT_TOKEN = os.environ.get("7156843846:AAFd-xHDZTrvWRD1gxd4K2DwKLXDQ0q2DSo")
+    TELEGRAM_API = os.environ["17075222"]
+    OWNER = os.environ.get("7103573952")
+    OWNER_USERNAME = os.environ.get("teststtt27")
+    PASSWORD = os.environ.get("1502194331aA$")
+    DATABASE_URL = os.environ.get("mongodb+srv://hi:hi@cluster0.rbbwg9i.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    LOGCHANNEL = os.environ.get("-1002135850719")  # Add channel id as -100 + Actual ID
     GDRIVE_FOLDER_ID = os.environ.get("GDRIVE_FOLDER_ID", "root")
     USER_SESSION_STRING = os.environ.get("USER_SESSION_STRING", None)
     IS_PREMIUM = False
